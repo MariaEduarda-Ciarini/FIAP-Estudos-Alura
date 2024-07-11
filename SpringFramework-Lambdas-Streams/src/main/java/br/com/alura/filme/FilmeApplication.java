fae07c668793d81c6103ee74d3651e57419aa04f
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2024-07-11 05:16:52 -0300

    Aprimorando Java e seu Framework
    https://cursos.alura.com.br/course/java-trabalhando-lambdas-streams-spring-framework

--- SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/FilmeApplication.java
+++ SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/FilmeApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.filme;
 
-import br.com.alura.filme.model.DadosSerie;
-import br.com.alura.filme.service.ConsumoApi;
-import br.com.alura.filme.service.ConverteDados;
+import br.com.alura.filme.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +14,27 @@ public class FilmeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t==Breaking+Bad&apikey=b1a75282");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
+
+//		var consumoApi = new ConsumoApi();
+//		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Stranger+Things&y=2016&apikey=b1a75282");
+//		System.out.println(json);
+//		ConverteDados conversor = new ConverteDados();
+//		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+//		System.out.println(dados);
+//		json = consumoApi.obterDados("https://www.omdbapi.com/?t=Stranger+Things&season=3&episode=2&apikey=b1a75282");
+//		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+//		System.out.println(dadosEpisodio);
+
+//		List<DadosTemporada> temporadas = new ArrayList<>();
+
+//		for (int i = 1; i<= dados.totalTemporadas(); i++){
+//			json = consumoApi.obterDados("https://www.omdbapi.com/?t=Stranger+Things&season="+ i +"&apikey=b1a75282");
+//		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+//		}
+//		temporadas.forEach(System.out::println);
 	}
 }
--- SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/model/DadosSerie.java
+++ SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/model/DadosSerie.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
-                         @JsonAlias("totalSeasons")Integer totalTemporadas,
-                         @JsonAlias("imdbRating")String avalicao) {
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avalicao) {
+
 
 }
 
