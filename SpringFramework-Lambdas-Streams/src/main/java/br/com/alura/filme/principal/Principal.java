515dc95c5fd321a2f2b84d9f0f3d667b4fb38d78
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2024-07-12 23:22:33 -0300

    estudando ainda...
    https://cursos.alura.com.br/course/java-trabalhando-lambdas-streams-spring-framework/task/135657

--- SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/principal/Principal.java
+++ SpringFramework-Lambdas-Streams/src/main/java/br/com/alura/filme/principal/Principal.java
@@ -1,22 +1,16 @@
 package br.com.alura.filme.principal;
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 import br.com.alura.filme.model.DadosEpisodio;
 import br.com.alura.filme.model.DadosSerie;
 import br.com.alura.filme.model.DadosTemporada;
+import br.com.alura.filme.model.Episodio;
 import br.com.alura.filme.service.ConsumoApi;
 import br.com.alura.filme.service.ConverteDados;
-<<<<<<< HEAD
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
-=======
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
->>>>>>> master
 
 
 public class Principal {
@@ -27,17 +21,10 @@ public class Principal {
 
     private ConverteDados conversor = new ConverteDados();
 
-<<<<<<< HEAD
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=b1a75282";
 
     public void exibeMenu() {
-=======
-    private  final String ENDERECO = "https://www.omdbapi.com/?t=";
-    private  final String API_KEY = "&apikey=b1a75282";
-
-    public void exibeMenu(){
->>>>>>> master
         System.out.println("Digite o nome da série para busca: ");
         var nomesSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomesSerie.replace(" ", "+") + API_KEY);
@@ -46,7 +33,6 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-<<<<<<< HEAD
         for (int i = 1; i <= dados.totalTemporadas(); i++) {
             json = consumo.obterDados(ENDERECO + nomesSerie.replace(" ", "+") + "&season=" + i + API_KEY);
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
@@ -79,6 +65,7 @@ public class Principal {
                 .flatMap(t -> t.episodios().stream())
                 .collect(Collectors.toList());
 
+
         System.out.println("\nTop 5 episódios");
         dadosEpisodios.stream()
                 .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
@@ -86,24 +73,31 @@ public class Principal {
                 .limit(5)
                 .forEach(System.out::println);
 
-=======
-		for (int i = 1; i<= dados.totalTemporadas(); i++){
-			json = consumo.obterDados(ENDERECO + nomesSerie.replace(" ", "+") +"&season=" +i+ API_KEY);
-		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
 
-		}
+        List<Episodio> episodios = new ArrayList<>();
+        for (DadosTemporada t : temporadas) {
+            for (DadosEpisodio d : t.episodios()) {
+                Episodio episodio = new Episodio(t.numero(), d);
+                episodios.add(episodio);
+            }
+        }
 
-		temporadas.forEach(System.out::println);
+        episodios.forEach(System.out::println);
 
-//        for (int i = 0; i< dados.totalTemporadas(); i ++ ) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for(int j = 0; j< episodiosTemporada.size(); j ++){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        int ano;
+        ano = leitura.nextInt();
+        leitura.nextLine();
 
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
->>>>>>> master
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                                "Temporada: " + e.getTemporada() +
+                                " Episodio: " + e.getTitulo()+
+                                " Data Lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
