242c45f1e345f14fe6b15a8deac836bbf7d2b0cb
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2024-08-09 03:23:03 -0300

    Java persistência de dados e consultas com Spring Data JPA
    82%

--- Java persistência de dados e consultas com Spring Data JPA/src/main/java/br/com/alura/filme/principal/Principal.java
+++ Java persistência de dados e consultas com Spring Data JPA/src/main/java/br/com/alura/filme/principal/Principal.java
@@ -33,6 +33,7 @@ public class Principal {
                      6 - Top 5 Séries
                      7 - Buscar séries por categoria
                      8 - Filtrar séries por temporada e avaliação
+                     9 - Buscar episodios por trecho
                                         
                      0 - Sair
                     """;
@@ -66,6 +67,9 @@ public class Principal {
                 case 8:
                     filtrarSeriesPorTemporadaEAvaliacao();
                     break;
+                case 9:
+                    buscarEpisodioPorTrecho();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -75,6 +79,17 @@ public class Principal {
         }
     }
 
+    private void buscarEpisodioPorTrecho() {
+        System.out.println("Qual nome do episodio para busca?");
+        var trechoEpisodio = leitura.nextLine();
+        List<Episodio> episodiosEncontrados = repositorio.buscarEpisodioPorTrecho(trechoEpisodio);
+        episodiosEncontrados.forEach(e ->
+                System.out.printf("Série: %s Temporada %s - Episódio %s - %s\n",
+                        e.getSerie().getTitulo(), e.getTemporada(),
+                        e.getNumeroEpisodio(), e.getTitulo()));
+
+    }
+
     private void filtrarSeriesPorTemporadaEAvaliacao() {
         System.out.println("Filtrar séries até quantas temporadas? ");
         var totalTemporadas = leitura.nextInt();
@@ -82,7 +97,7 @@ public class Principal {
         System.out.println("Com avaliação a partir de que valor? ");
         var avaliacao = leitura.nextDouble();
         leitura.nextLine();
-        List<Serie> filtroSeries = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporadas, avaliacao);
+        List<Serie> filtroSeries = repositorio.seriesPorTemporadaEAvaliacao(totalTemporadas, avaliacao);
         System.out.println("*** Séries filtradas ***");
         filtroSeries.forEach(s -> System.out.println(s.getTitulo() + "  - avaliação: " + s.getAvaliacao()));
     }
--- Java persistência de dados e consultas com Spring Data JPA/src/main/java/br/com/alura/filme/repository/SerieRepository.java
+++ Java persistência de dados e consultas com Spring Data JPA/src/main/java/br/com/alura/filme/repository/SerieRepository.java
@@ -1,17 +1,31 @@
 package br.com.alura.filme.repository;
 
 import br.com.alura.filme.model.Categoria;
+import br.com.alura.filme.model.Episodio;
 import br.com.alura.filme.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
     Optional<Serie> findByTituloContainingIgnoreCase(String titulo);
+
     List<Serie> findByAtoresContainingIgnoreCase(String ator);
+
     List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
     List<Serie> findByGenero(Categoria categoria);
+
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
 
+//    @Query(value = "select * from series WHERE series.total_temporadas <= 7 AND series.avaliacao >= 7.5", nativeQuery = true)
+//    List<Serie> seriesPorTemporadaEAvaliacao();
+
+    @Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaEAvaliacao(int totalTemporadas, double avaliacao);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
+    List<Episodio> buscarEpisodioPorTrecho(String trechoEpisodio);
 }
