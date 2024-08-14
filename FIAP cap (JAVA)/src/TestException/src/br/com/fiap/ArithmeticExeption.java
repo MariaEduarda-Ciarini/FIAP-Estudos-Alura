6f1029a1317abedd3c1782d9f6125d053b3cc3d9
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2024-08-14 11:34:34 -0300

    delete

--- FIAP cap (JAVA)/src/TestException/src/br/com/fiap/ArithmeticExeption.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap;
-
-public class ArithmeticExeption extends Exception {
-
-}
--- FIAP cap (JAVA)/src/TestException/src/br/com/fiap/DivisionbyZeroExcepetion.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap;
-
-public class DivisionbyZeroExcepetion extends Exception {
-
-}
