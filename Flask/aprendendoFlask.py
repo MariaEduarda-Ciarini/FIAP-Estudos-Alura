8119a931cf38f2cff5281fb451cea6fd88ce66bd
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2025-01-08 14:28:03 -0300

    05.Autenticando usuários com sessão do Flask
    Curso de
    Flask: crie uma webapp com Python

--- Flask/aprendendoFlask.py
+++ Flask/aprendendoFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 class   Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,15 +6,29 @@ class   Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 =Jogo('Max Payne 2', 'Detetive', 'PC, XBOX360, PS2')
+jogo2 =Jogo('Bully', 'Aventura', 'PS2, XBOX, PC, NINTENDO')
+jogo3 =Jogo('Skate 3', 'Esportivo', 'XBOX')
+lista = [jogo1, jogo2, jogo3]
+
 
 app = Flask(__name__)
-@app.route('/inicio')
-def ola():
-    jogo1 =Jogo('Max Payne 2', 'Detetive', 'PC, XBOX360, PS2')
-    jogo2 =Jogo('Bully', 'Aventura', 'PS2, XBOX, PC, NINTENDO')
-    jogo3 =Jogo('Skate 3', 'Esportivo', 'XBOX')
-
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo= 'Jogos', jogos=lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form ['categoria']
+    console = request.form ['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+
+app.run(debug=True)
